a658cdbf63c8f8fbbf9a2ad483a8264a2fc8fca4
Daniel Salib <Daniel@Daniels-MacBook-Air.local>
2017-08-07 18:44:06 -0400

    untracked xcode files

---
+++ IosNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IosNotes
+//
+//  Created by Daniel Salib on 2017-08-07.
+//  Copyright © 2017 Daniel Salib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
